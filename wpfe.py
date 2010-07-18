bdac9b03697bd01216503e31de5c1d76809acc5d
cipher16 <cipher16@cipher16.(none)>
2010-07-18 17:14:06 +0200

    Ajout d'un README pour l'install et le TODO

--- wpfe.py
+++ wpfe.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-from Controllers.WPImporter import *
+from Controllers.WPAdmin import *
 from Controllers.RSSContentUpdater import RssContentUpdater
 from Controllers.Dispatcher import *
 import os
@@ -17,9 +17,10 @@ TEMPLATE = APPLICATION_PATH+"/Views/default"
 
 application = webapp.WSGIApplication(
     [
-      ('/admin/upload',WPUploader),                    #import 0%
+      ('/admin/upload',WPUploader),                    #import 100%
       ('/admin/import',WPImporter),                    #import 100% 
       ('/admin/rssUpdate', RssContentUpdater),         #50% parsing OK, need DB insertion + cron
+      ('/admin/',WPAdmin),                    #import 100% 
       ('/', Home),                  #display several articles OK 100%
       ('/page/.*', Home),           #display page contents    OK 100%
       ('/tag/.*', TagsAndCats),     #archive system                0%
