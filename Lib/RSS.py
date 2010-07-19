6bcb8fc2d13ef7ed629a7ab3a73fcc2ba406e8ff
cipher16 <cipher16@bureau.(none)>
2010-07-19 22:11:17 +0200

    Verification de la date vis à vis du refresh du flux

--- Lib/RSS.py
+++ Lib/RSS.py
@@ -1,10 +1,14 @@
 from Models.RSS import RSSContent
 from google.appengine.api import urlfetch
+import wpfe
+import datetime
 
 def getFeedUrl(url):
     t = RSSContent.all().filter("url", url).order("-date").fetch(1, 0)
     if t :
-        return t[0].content
+        t = t[0]
+        if t.date+datetime.timedelta(seconds=wpfe.FEED_REFRESH) > datetime.datetime.today():#si ca date de moins d'un jour on prend de la base sinon, ...
+            return t.content
     t = urlfetch.Fetch(url=url).content
     feed = RSSContent()
     feed.content=t
