02eb7f8fdcd77f16e09f3ba51c0b188c09dac3fc
cipher16 <cipher16@bureau.(none)>
2010-07-21 19:24:04 +0200

    Fucking noob

--- wpfe.py
+++ wpfe.py
@@ -34,7 +34,7 @@ application = webapp.WSGIApplication(
       ('/page/.*', Home),           #display page contents    OK 100%
       ('/tag/.*', TagsAndCats),     #archive system              100%
       ('/category/.*', TagsAndCats),#archive system              100%
-      ('/feed.*', Feed),           #feed generator               90% recuperation depuis la base de données ?
+      ('/feed.*', Feed),           #feed generator               90% recuperation depuis la base de donnees ?
       ('/wp-content.*',CDN),
       ('/.*', Dispatcher)           #display single article   OK 100%
     ],debug=True)
